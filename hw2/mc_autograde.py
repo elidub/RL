3816f88f96605bf367ad180778ec7a5e08117688
Elias Dubbeldam <eliasdubbeldam@gmail.com>
2022-09-22 10:43:14 +0200

    finished part 1, still unsure about part 2

--- hw2/mc_autograde.py
+++ hw2/mc_autograde.py
@@ -115,7 +115,7 @@ def mc_prediction(env, policy, num_episodes, discount_factor=1.0, sampling_funct
     # YOUR CODE HERE
     for _ in tqdm(range(num_episodes)):
         states, actions, rewards, dones = episode = sample_episode(env, policy)
-        G = 0
+        G = 0.
 #         for state, action, reward, done in zip(*episode):
         reversed_episodes = zip(reversed(states), reversed(actions), reversed(rewards), reversed(dones))
         for t, (state, action, reward, done) in enumerate(reversed_episodes):
@@ -124,5 +124,93 @@ def mc_prediction(env, policy, num_episodes, discount_factor=1.0, sampling_funct
 #                 print(returns_count, returns_count[state])
                 returns_count[state] += 1
                 V[state] = (returns_count[state] * V[state] + G) / (returns_count[state] + 1)
+    
+    return V
+
+class RandomBlackjackPolicy(object):
+    """
+    A random BlackJack policy.
+    """
+    def get_probs(self, states, actions):
+        """
+        This method takes a list of states and a list of actions and returns a numpy array that contains 
+        a probability of perfoming action in given state for every corresponding state action pair. 
+
+        Args:
+            states: a list of states.
+            actions: a list of actions.
+
+        Returns:
+            Numpy array filled with probabilities (same length as states and actions)
+        """
+        # YOUR CODE HERE
+        
+        probs = [0.5] * len(actions)
+
+#         for i, (state, action) in enumerate(zip(states, actions)):
+            
+#             sampled_action = self.sample_action(state)
+            
+#             probs[i] = (sampled_action == action)*1.
+            
+        return np.array(probs)
+    
+    def sample_action(self, state):
+        """
+        This method takes a state as input and returns an action sampled from this policy.  
+
+        Args:
+            state: current state
+
+        Returns:
+            An action (int).
+        """
+        # YOUR CODE HERE
+        
+        stick, hit = 0, 1
+        action = np.random.choice([stick, hit])
+        
+        return action
+
+def mc_importance_sampling(env, behavior_policy, target_policy, num_episodes, discount_factor=1.0,
+                           sampling_function=sample_episode):
+    """
+    Monte Carlo prediction algorithm. Calculates the value function
+    for a given target policy using behavior policy and ordinary importance sampling.
+    
+    Args:
+        env: OpenAI gym environment.
+        behavior_policy: A policy used to collect the data.
+        target_policy: A policy which value function we want to estimate.
+        num_episodes: Number of episodes to sample.
+        discount_factor: Gamma discount factor.
+        sampling_function: Function that generates data from one episode.
+    
+    Returns:
+        A dictionary that maps from state -> value.
+        The state is a tuple and the value is a float.
+    """
+
+    # Keeps track of current V and count of returns for each state
+    # to calculate an update.
+    V = defaultdict(float)
+    returns_count = defaultdict(float)
+    
+    # YOUR CODE HERE
+    for _ in tqdm(range(num_episodes)):
+#         behavior_policy = target_policy
+        states, actions, rewards, dones = episode = sample_episode(env, behavior_policy)
+        G = 0.
+        W = 1.
+        reversed_episodes = zip(reversed(states), reversed(actions), reversed(rewards), reversed(dones))
+        for t, (state, action, reward, done) in enumerate(reversed_episodes):
+            G = discount_factor * G + reward
+            pi, b = target_policy.get_probs([state], [action]), behavior_policy.get_probs([state], [action])
+            
+            W = W * pi / b # Already calculate W_{n+1}
+            V[state] = V[state] + 1 / (returns_count[state] + 1) * (W * G - V[state])
+
+            if W == 0:
+                break
     
     return V
